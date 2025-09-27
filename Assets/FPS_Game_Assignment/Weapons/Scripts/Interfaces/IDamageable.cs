262ef37c84971b433201512843dbafc35da2989a
Ahmad Raza <ahmadraza.2aa1i@gmail.com>
2025-09-27 15:49:45 +0500

    Revert "Dummy pistol weapon added" Undo
    This reverts commit 22706d8d7351a624efd8e1926fc95528165f5926.

--- Assets/FPS_Game_Assignment/Weapons/Scripts/Interfaces/IDamageable.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-public interface IDamageable
-{
-    /// <summary>Apply damage. Return true if object died/was destroyed (optional).</summary>
-    bool ApplyDamage(float amount, RaycastHit? hit = null);
-}
