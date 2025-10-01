d44ffa09bfd1fb3a972cb782ec61a751a858156b
Ahmad Raza <ahmadraza.2aa1i@gmail.com>
2025-10-01 22:52:18 +0500

    fixes

--- Assets/FPS_Game_Assignment/Enemy/Scripts/EnemySensor.cs
+++ Assets/FPS_Game_Assignment/Enemy/Scripts/EnemySensor.cs
@@ -1,10 +1,6 @@
 using System;
 using UnityEngine;
 
-/// <summary>
-/// Small trigger-based sensor. Raises events when an object on configured layers enters/exits.
-/// Keeps sensing responsibility out of states and enemy logic (SRP).
-/// </summary>
 [RequireComponent(typeof(Collider))]
 public class EnemySensor : MonoBehaviour
 {
@@ -15,7 +11,7 @@ public class EnemySensor : MonoBehaviour
 
     private Transform _currentTarget;
 
-    private SphereCollider col;
+    private Collider col;
     private void Awake()
     {
         // ensure collider is a trigger for OnTrigger events
@@ -25,7 +21,9 @@ public class EnemySensor : MonoBehaviour
     }
     public void ConfigureRadius(float radius)
     {
-        col.radius = radius;
+        SphereCollider col = GetComponent<SphereCollider>();
+        if (col != null)
+            col.radius = radius;
     }
     private void OnTriggerEnter(Collider other)
     {
